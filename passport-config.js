10e035d0ba6df27237be74919c85b027ef26a88c
Tankun Jeenyoung <the.tankunth@gmail.com>
2021-11-09 14:51:23 +0700

    change to heroku

--- passport-config.js
+++
@@ -1,8 +0,0 @@
-// const localstrategy = require("passport-local").Strategy;
-// const bcrypt = require("bcrypt");
-
-// const initializePassport = async (Passport, getUserByUsername) => {
-
-// }
-
-// module.exports = initializePassport;
